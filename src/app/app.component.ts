89b02ffeaff126a921edaf8e1e6f1c690bab6d88
SantiagoIlli <santiagoilli96@gmail.com>
2020-11-20 18:12:13 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'create-ml';
+
+  @Input() inputVar: string;
+  @Output() testOutput = new EventEmitter<any>();
+
+
+  @Input() public form: FormGroup;
+  @Input() public infoFormGroup: FormGroup;
+  @Input() public exercisesQuantity: number;
+  public triviaTypes: { name: string, value: 'classic' | 'test' }[];
+
+
+  settingsFormGroup: FormGroup;
+
+
+  onClickSendOutput(): void {
+    this.testOutput.emit({
+      something: 'this is something from creator'
+    });
+  }
+
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.infoFormGroup = this._formBuilder.group({
+      name: [' game name ', [Validators.required, Validators.maxLength(256)]],
+      // todo validator de max peso
+      image: [''],
+      // tags: [this.game.tags || []],
+      // language: [this.game.language || 'es'],
+      // level: [this.game.level || 1],
+      description: ['game description', [Validators.required, Validators.maxLength(500)]]
+    });
+    this.settingsFormGroup = this._formBuilder.group({
+      triviaType: ['classic'],
+      host: ['gauss'],
+      isRandom: [false],
+      goal: [6,
+        [Validators.min(5), Validators.max(20), Validators.required]],
+      /* minPlayers: [this.game.settings ? this.getSettingsPropertyValueOf('minPlayers', '1') : 1,
+        [Validators.min(1), Validators.max(8), Validators.required]],
+      maxPlayers: [this.game.settings ? this.getSettingsPropertyValueOf('maxPlayers', '8') : 8,
+        [Validators.min(1), Validators.max(8), Validators.required]], */
+    });
+  }
+
+  ngOnInit(): void {
+    this.triviaTypes = [{name: 'Clásico', value: 'classic'}, {name: 'Examen', value: 'test'}];
+  }
+
+  updateHost(imagePath: string): void {
+    this.form.get('host').patchValue(imagePath);
+    this.form.get('host').markAsDirty();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {Injector, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FlexModule} from '@angular/flex-layout';
+import {createCustomElement} from '@angular/elements';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+  // entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const myElement = createCustomElement(AppComponent, { injector });
+    customElements.define('creator-ml', myElement);
+  }
+  // ngDoBootstrap(): void {}
+}
